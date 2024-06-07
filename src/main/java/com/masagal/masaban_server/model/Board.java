30601553671750b9be16fb1f345d852c8e39a4a8
Simon Kendall <simon.a.kendall@gmail.com>
2024-06-07 15:06:24 +0200

    implement adding and retrieving cards

--- src/main/java/com/masagal/masaban_server/model/Board.java
+++ src/main/java/com/masagal/masaban_server/model/Board.java
@@ -1,20 +1,25 @@
 package com.masagal.masaban_server.model;
 
 import com.masagal.masaban_server.services.BoardService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class Board {
+    private final Logger logger = LogManager.getLogger();
     private final ArrayList<Card> cards;
     private final int id;
     private static int assignedIds;
+    private int assignedCardIds = 0;
 
     public Board() {
         cards = new ArrayList<Card>();
-        this.id = ++assignedIds;
+        this.id = assignedIds++;
     }
 
     public void add(Card newCard) {
@@ -34,11 +39,20 @@ public class Board {
     }
 
     public Card createCard(String contents) {
-        return null;
+        Card card = new Card(contents, ++assignedCardIds);
+        cards.add(card);
+        logger.debug("created card with id {} and contents {}", card.id(), card.text());
+        return card;
     }
 
-    public Card getCardById(int id) {
-        return null;
+    public Card getCardById(int cardId) {
+        Optional<Card> result = cards.stream().filter((x) -> x.id() == cardId).findFirst();
+        if(result.isEmpty()) {
+            logger.error("Card requested with id {} does not exist", cardId);
+
+            throw new IllegalArgumentException("Card with id does not exist: "+cardId);
+        }
+        return result.get();
     }
 
     public Card updateCard(int id, String contents) {
