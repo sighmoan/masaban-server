c0e7c39f4de163ff5834113aff69756b906a2e56
Simon Kendall <simon.a.kendall@gmail.com>
2024-06-07 13:46:54 +0200

    add tests for board

---
+++ src/test/java/com/masagal/masaban_server/model/BoardTest.java
@@ -0,0 +1,17 @@
+package com.masagal.masaban_server.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+    Board board;
+
+    @Test
+    void shouldGenerateUniqueId() {
+        // Arrange
+        // Act
+        // Assert
+        
+    }
+}
