72377466f04d0db6ed8870529c9650ec71f3b0a7
Simon Kendall <simon.a.kendall@gmail.com>
2024-06-11 08:44:29 +0200

    add integration test for card creation

--- src/test/java/com/masagal/masaban_server/MvcIntegrationTests.java
+++ src/test/java/com/masagal/masaban_server/MvcIntegrationTests.java
@@ -1,17 +1,13 @@
 package com.masagal.masaban_server;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.hamcrest.Matchers.isEmptyString;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,6 +25,20 @@ public class MvcIntegrationTests {
         //Act
         //Assert
         mockMvc.perform(post("/api/v1/board"))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("location"))
+                .andReturn();
+
+    }
+
+    @Test
+    void canCreateNewCard() throws Exception {
+        //Arrange
+        MvcResult result = mockMvc.perform(post("/api/v1/board")).andReturn();
+        String location = result.getResponse().getHeader("location");
+        //Act
+        //Assert
+        mockMvc.perform(post(location + "/card"))
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("location"));
     }
