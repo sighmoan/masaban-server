1849b7ee50322eb012034d9cba5cfd4b074ae0eb
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-01 16:52:31 +0200

    add board repo tests

--- src/test/java/com/masagal/masaban_server/BoardRepoIntegrationTest.java
+++ src/test/java/com/masagal/masaban_server/BoardRepoIntegrationTest.java
@@ -1,10 +1,7 @@
 package com.masagal.masaban_server;
 
 import com.masagal.masaban_server.domain.BoardService;
-import com.masagal.masaban_server.model.Board;
-import com.masagal.masaban_server.model.BoardRepository;
-import com.masagal.masaban_server.model.CardRepository;
-import com.masagal.masaban_server.model.ColumnRepository;
+import com.masagal.masaban_server.model.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +10,9 @@ import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -41,5 +41,31 @@ public class BoardRepoIntegrationTest {
 
         assertFalse(service.getCardsByColumn(firstColumnId).isEmpty());
 
+    }
+
+    @Test
+    void canDeleteCard() {
+        service.createCardOnColumn(boardId, firstColumnId);
+        UUID cardId = service.createCardOnColumn(boardId, firstColumnId);
+
+        service.deleteCardOnBoard(boardId, cardId);
+        assertEquals(1, service.getCardsByColumn(firstColumnId).size());
+    }
+
+    @Test
+    void canMoveCardToAnotherColumn() {
+        List<Column> cols = service.getColumns(boardId);
+        service.createCardOnColumn(boardId, cols.get(0).getId());
+
+        UUID cardId = service.createCardOnColumn(boardId, cols.get(0).getId());
+        service.moveCard(cardId, cols.get(1).getId());
+
+        assertEquals(1, service.getCardsByColumn(cols.get(0).getId()).size());
+        assertEquals(1, service.getCardsByColumn(cols.get(1).getId()).size());
+    }
+
+    @Test
+    void canUpdateContentOfCard() {
+        fail();
     }
 }
