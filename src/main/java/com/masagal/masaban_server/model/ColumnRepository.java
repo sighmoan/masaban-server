7f51e733d9542af10b47537bf9bc67c86768d180
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-01 16:53:44 +0200

    declare method to find columns by cards

--- src/main/java/com/masagal/masaban_server/model/ColumnRepository.java
+++ src/main/java/com/masagal/masaban_server/model/ColumnRepository.java
@@ -3,8 +3,10 @@ package com.masagal.masaban_server.model;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ColumnRepository extends CrudRepository<Column, UUID> {
+    Column findByCardsContaining(List<Card> cards);
 }
